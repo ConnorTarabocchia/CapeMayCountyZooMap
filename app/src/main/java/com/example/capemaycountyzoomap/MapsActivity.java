aa2ec90bd2bfc8f76306c434409c23af5752655b
Connor Tarabocchia <tarabocc@go.stockton.edu>
2019-04-24 18:59:43 -0400

    need to fine tune some things, also need to get permissions working and gps position

--- app/src/main/java/com/example/capemaycountyzoomap/MapsActivity.java
+++ app/src/main/java/com/example/capemaycountyzoomap/MapsActivity.java
@@ -1,15 +1,11 @@
 package com.example.capemaycountyzoomap;
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.res.Resources;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
-import android.support.v7.app.AlertDialog;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,7 +16,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,12 +24,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+        } else {
+            // Permission has already been granted
+        }
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
     /**
@@ -52,26 +68,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap = googleMap;
 
-        LatLng zoo = new LatLng(39.10, -74.81); //lat and lng of cape may county zoo
+        LatLng zoo = new LatLng(39.103, -74.816); //lat and lng of cape may county zoo
         LatLng pic = new LatLng(39.101451, -74.815200); //lat and lng for the picture overlay
         LatLng backing = new LatLng(39.101954,-74.8152057); //lat and lng for the background overlay
 
-
         mMap.moveCamera(CameraUpdateFactory.newLatLng(zoo));  //moves camera to the coordinates of zoo
         mMap.getUiSettings().setRotateGesturesEnabled(false);   //prevents user from rotating with a gesture
-        mMap.setMinZoomPreference(16.2f);  //minimum zoom level so user cant see outside zoo boundaries
+        mMap.setMinZoomPreference(16.7f);  //minimum zoom level so user cant see outside zoo boundaries
         mMap.getUiSettings().setCompassEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
-        //checks or asks for permissions, specifically if location permissions are available,
-        //if they are, enable the current location on the map
-        /*
-        if (android.os.Build.VERSION.SDK_INT > 23) {  }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+        } catch (Resources.NotFoundException e) {
+        }
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
             mMap.setMyLocationEnabled(true);
-        } else {
+        } catch (SecurityException e) { }
 
-        }  */
         GroundOverlayOptions backgroundGoo = new GroundOverlayOptions()      //used for overlaying the image
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.backdrop))        //calls the bitmap image bmp
                 .position(backing, 1600f, 1600f)       //Sets the size of the image to be displayed
@@ -103,5 +124,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //sets default zoom level, the higher the number the closer the zoom
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoo, 17));
+
+
     }
 }
